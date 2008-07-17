03fb15bac2021a094d6124a4a791e2213f6b81ac
Lee Olayvar <leefmcpub+gitpub@gmail.com>
2008-07-16 19:37:35 -0700

    More toying with PyGTK, with basic GUI layout forming.

--- gui/external_gui.py
+++ gui/external_gui.py
@@ -3,7 +3,6 @@
 '''
 A gui to standardize the testing of component creation.
 '''
-
 # standard
 # related
 import pygtk
@@ -41,19 +40,103 @@ class ExternalGUI(object):
         
         # Create the Tables for the tabs.
         component_table = gtk.Table(16, 6, True)
+        component_table.show()
+        
+        # Create component name field
+        self.create_component_name_entry = gtk.Entry(
+            max=20
+        )
+        self.create_component_name_entry.set_text('HCRF_Component')
+        component_table.attach(self.create_component_name_entry, 0, 6, 0, 1)
+        self.create_component_name_entry.connect(
+            'activate', self.component_name_entered)
+        self.create_component_name_entry.show()
+        
+        # Create Component Button
+        button = gtk.Button('Create Component')
+        component_table.attach(button, 3, 6, 1, 2)
+        button.show()
+        
+        # Create the menu widgets (Do not .show())
+        self.choose_component_menu = gtk.Menu()
+        
+        # Currently no component lookup code is programmed, so use these fakes.
+        categories = (
+            {'name':'Fake Category',
+             'components':(
+                 {'name':'Comp A',
+                  'path':'compa'},
+                 {'name':'Comp B',
+                  'path':'compb'},
+                 {'name':'Comp C',
+                  'path':'compc'},
+             )},
+            {'name':'Other Category',
+             'components':(
+                 {'name':'Comp A',
+                  'path':'compa'},
+                 {'name':'Comp B',
+                  'path':'compb'},
+                 {'name':'Comp C',
+                  'path':'compc'},
+             )},
+        )
+        
+        for category in categories:
+            category_item = gtk.MenuItem(category['name'])
+            self.choose_component_menu.append(category_item)
+            
+            category_submenu = gtk.Menu()
+            for component in category['components']:
+                component_item = gtk.MenuItem(component['name'])
+                component_item.connect(
+                    'activate',
+                    self.choose_component_item_response,
+                    component
+                )
+                category_submenu.append(component_item)
+                component_item.show()
+
+            category_item.show()
+            category_item.set_submenu(category_submenu)
+        
+        # Select Component Button
+        self.choose_component_menu_button = gtk.Button('Choose a Component')
+        component_table.attach(self.choose_component_menu_button,
+                               0, 3, 1, 2)
+        self.choose_component_menu_button.connect_object(
+            'event', self.choose_component_pressed,
+            self.choose_component_menu)
+        self.choose_component_menu_button.show()
         
         # Append the tables to the tabs.
         notebook.append_page(component_table, gtk.Label('Test'))
-        
-        # Create first button
-        button = gtk.Button('Create Component')
-        component_table.attach(button, 0, 3, 0, 1)
-        button.show()
-
-        component_table.show()
         notebook.show()
         self.window.show()
 
+    def choose_component_item_response(self, widget, data):
+        '''
+        '''
+        print 'Data: %s' % str(data)
+        self.choose_component_menu_button.set_label('Comp: %s' % data['name'])
+    
+    def choose_component_pressed(self, widget, event):
+        '''
+        '''
+        if event.type == gtk.gdk.BUTTON_PRESS:
+            widget.popup(None, None, None, event.button, event.time)
+            # Tell calling code that we have handled this event the buck
+            # stops here.
+            return True
+        # Tell calling code that we have not handled this event pass it on.
+        return False
+    
+    def component_name_entered(self, widget):
+        '''
+        '''
+        # Replace spaces with underscores.
+        widget.set_text(widget.get_text().replace(' ', '_'))
+    
     def delete_event(self, widget, event, data=None):
         '''
         @return: Returns False to call L{self.destroy}
@@ -71,3 +154,4 @@ class ExternalGUI(object):
 if __name__ == "__main__":
     gui = ExternalGUI()
     gui.main()
+
